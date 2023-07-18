73e7322328e50f38cb07e548b45b0e087e5bbc90
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2023-07-19 09:16:59 +1200

    Changed title to my crazy website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,16 @@
-<h1>G'day my amigo!</h1>
-<p1>woahzasr</p1>
-<p2>This is crazy</p2>
+<h1>My Crazy Website</h1>
+<p>woahzasr</p>
+<p>This is crazy</p>
+<p>This is crazy</p>
+<p>This is crazy</p>
+<p>This is crazy</p>
+<p>This is not crazy</p>
+<p>This is crazy</p>
+<p>This is crazy</p>
+<p>This is supa crazy</p>
+<p>This is crazy</p>
+<p>This is crazy</p>
+<p>This is crazy</p>
+<h3>This crazy</h3>
+<p>This is crazy</p>
+<p>This is crazy</p>
