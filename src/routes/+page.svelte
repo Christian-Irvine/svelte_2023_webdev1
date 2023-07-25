51c3645da0cc5032f7991ebfd00214df42ad8011
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2023-07-26 09:49:44 +1200

    Added iframe to my channel

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,10 +5,14 @@
         <li>Awesomeness</li>
         <li>Super Awesomeness</li>
         <li>Excellent</li>
-        <li>Amazing</li>
+        <li><b>Amazing</b></li>
     </ul>
 
-    <a href="https://www.horse.com" target="_blank"><img src="https://res.cloudinary.com/dk-find-out/image/upload/q_80,w_1920,f_auto/MA_00600259_vpf1m0.jpg" alt="Epic Horse" width="1850"/></a>
+    <button type="button" name ="Tommy The Button"> Yo imma button</button>
+
+    <a href="https://www.horse.com" target="_blank"><img src="https://res.cloudinary.com/dk-find-out/image/upload/q_80,w_1920,f_auto/MA_00600259_vpf1m0.jpg" alt="Epic Horse" width="1850" height ="650"/></a>
+
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/ekBDeKwy86I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
     <p><a href="https://youtube.com/craftymcfish" target="_blank">Lets go on an adventure!</a></p>
 
