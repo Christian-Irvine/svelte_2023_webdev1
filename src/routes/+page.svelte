ad964ca6eee91143d8d026e5787d8d6989593126
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2023-07-26 09:25:29 +1200

    Made horse image lead to horse.com

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,8 +8,12 @@
         <li>Amazing</li>
     </ul>
 
-    <img src="https://res.cloudinary.com/dk-find-out/image/upload/q_80,w_1920,f_auto/MA_00600259_vpf1m0.jpg" alt="Epic Horse" width="1850"/>
+    <a href="https://www.horse.com" target="_blank"><img src="https://res.cloudinary.com/dk-find-out/image/upload/q_80,w_1920,f_auto/MA_00600259_vpf1m0.jpg" alt="Epic Horse" width="1850"/></a>
 
     <p><a href="https://youtube.com/craftymcfish" target="_blank">Lets go on an adventure!</a></p>
 
 </div>
+
+<footer>
+    <a href="https://discord.gg/2rkceDa8" target="_blank">Discord</a>
+</footer>
