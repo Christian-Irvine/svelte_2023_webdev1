8ff74cc4fa15ce1a84754d157ffcff4af1fe36f8
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2023-07-28 11:53:05 +1200

    All of week 2 session 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,91 @@
-<div>
+<style>
+    body{
+        margin: 0px;
+        padding: 0;
+    }
+
+    h1 {
+        text-align: center; 
+        color: magenta;
+        font-family: "Bahnschrift", Helvetica, sans-serif;
+        font-size: 120px;
+        margin: 0;
+    }
+        
+
+    h3 {
+        margin: 0;
+        text-align: center; 
+        color: -webkit-linear-gradient(magenta, brown);
+        font-family: "Bahnschrift", Helvetica, sans-serif;
+        font-size: 56px;
+    }
+
+    p {
+        margin: 0;
+        text-align: center; 
+        color: purple;
+        font-family: "Bahnschrift", Helvetica, sans-serif;
+    }
+
+    div {
+        text-align: center; 
+        background-image: linear-gradient(yellow, red);
+        margin: 0;
+    }
+
+    ul{
+        list-style-position: inside;
+        padding:0;
+    }
+
+    li {
+        margin: 0;
+        text-align: center; 
+        color: purple;
+        font-family: "Bahnschrift", Helvetica, sans-serif;
+    }
+        
+    a {
+        color: greenyellow;
+    }
+
+    header {
+            height: 150px; 
+            background-size: cover; 
+            background-repeat: no-repeat;
+            background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYxzUkbpydrDeIMWkPZZzaz2EwtJTGGRAP3A&usqp=CAU");
+            font-family: "Bahnschrift", Helvetica, sans-serif;
+            margin: 0;
+        }
+
+    footer {
+            background-color: cadetblue;
+            font-family: "Bahnschrift", Helvetica, sans-serif;
+            margin: 0;
+        }
+
+    button {
+            text-align: center;
+            background-color: greenyellow;
+            color: orangered;
+            font-family: "Bahnschrift", Helvetica, sans-serif;
+            
+        }
+    button:hover{
+        text-align: right;
+        background-color: hotpink;
+            color: blue;
+    }
+
+</style>
+
+<header>
     <h1>Welcome To My Sweet As Website</h1>
+</header>
+
+<div>
+    
 
     <ul>
         <li>Awesomeness</li>
@@ -8,16 +94,22 @@
         <li><b>Amazing</b></li>
     </ul>
 
-    <button type="button" name ="Tommy The Button"> Yo imma button</button>
+    <a href="https://www.james.com" target="_blank"><button type="button" name ="James The Button"> Yo i'm James the button</button></a>
 
-    <a href="https://www.horse.com" target="_blank"><img src="https://res.cloudinary.com/dk-find-out/image/upload/q_80,w_1920,f_auto/MA_00600259_vpf1m0.jpg" alt="Epic Horse" width="1850" height ="650"/></a>
+    <a href="https://www.horse.com" target="_blank"><img src="https://res.cloudinary.com/dk-find-out/image/upload/q_80,w_1920,f_auto/MA_00600259_vpf1m0.jpg" alt="Epic Horse" width="1850px" height ="650px"/></a>
 
+    <h3>Yo look at this video</h3>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/ekBDeKwy86I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
     <p><a href="https://youtube.com/craftymcfish" target="_blank">Lets go on an adventure!</a></p>
 
-</div>
 
+    <a href="https://www.google.com" target="_blank"><img src="https://blog.photoshelter.com/wp-content/uploads/2018/09/google-images-crop-1.png" alt="Image" height ="200px"/></a>
+    <a href="https://www.google.com" target="_blank"><img src="https://blog.photoshelter.com/wp-content/uploads/2018/09/google-images-crop-1.png" alt="Image hehe" height ="200px"/></a>
+    <a href="https://www.google.com" target="_blank"><img src="https://blog.photoshelter.com/wp-content/uploads/2018/09/google-images-crop-1.png" alt="Image" height ="200px"/></a>
+    <a href="https://www.google.com" target="_blank"><img src="https://blog.photoshelter.com/wp-content/uploads/2018/09/google-images-crop-1.png" alt="Image" height ="200px"/></a>
+
+</div>
 <footer>
     <a href="https://discord.gg/2rkceDa8" target="_blank">Discord</a>
 </footer>
