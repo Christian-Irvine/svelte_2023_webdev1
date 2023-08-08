96528bc9587ab2315cdc9f3501cd628eeb55ca14
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2023-08-09 09:32:46 +1200

    Tweeked the colour and size of borders for images

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,25 +2,8 @@
 
 <p>Come and see the horses!</p>
 <p>We have lots of horses</p>
-<p>Thats actually all we have right about now</p>
+<p>Horses are actually all we have right about now</p>
 <p>One day we might have more than horses</p>
 <p>But for now we have horses</p>
 <a href="https://www.horse.com" target="_blank"><p>Neigh</p></a>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+
