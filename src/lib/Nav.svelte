1970d84373db061622969cb07055d36cfdfe16bc
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2023-08-25 11:39:19 +1200

    Changed horses page to be a gallery about me and look nice using grid not flex

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -2,6 +2,6 @@
     <a href="/"><button>Home</button></a>
     <a href="/craftymcfish"><button>Crafty</button></a>
     <a href="/chrissyfresh"><button>Chrissy</button></a>
-    <a href="/horses"><button>Horse</button></a>
+    <a href="/gallery"><button>Gallery</button></a>
 </nav>
 
---
+++ src/routes/gallery/+page.svelte
@@ -0,0 +1,17 @@
+<div class="gallery">
+    <div>
+        <h2>Me Being Awesome</h2>
+        <div>
+            <img class="border" src="craftymcfish1.jpg" alt="Me being awesome">
+            <p class="center_vertically">This person is beautiful, look how he gallops</p>            
+        </div>
+    </div>
+
+    <div>
+        <h2>Me Being Dope As</h2>
+        <div>
+            <img class="border" src="craftymcfish1.jpg" alt="Me being dope as">
+            <p class="center_vertically">This fish is wonderful, look how he runs!</p>            
+        </div>
+    </div>
+</div>
