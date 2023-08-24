94672dbc951c53b768a2b683ce79502c0fb93616
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2023-08-25 11:51:03 +1200

    Added a new image and moved text to oposite sides in gallary

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -3,15 +3,17 @@
         <h2>Me Being Awesome</h2>
         <div>
             <img class="border" src="craftymcfish1.jpg" alt="Me being awesome">
-            <p class="center_vertically">This person is beautiful, look how he gallops</p>            
+            <p class="center_vertically">This is an image of me on my first day of school, I was so exited and overjoyed to be going to school that day I nearly threw up!
+                However, I didn't throw up after all and made it to school, yipee!
+            </p>            
         </div>
     </div>
 
     <div>
         <h2>Me Being Dope As</h2>
         <div>
-            <img class="border" src="craftymcfish1.jpg" alt="Me being dope as">
-            <p class="center_vertically">This fish is wonderful, look how he runs!</p>            
+            <img class="border" src="100DaysEpisode1.png" alt="Me being dope as">
+            <p class="center_vertically">This was my first day on my 100 Days Challenege run! Look how eager and ready I am to get started and win the game. I wonder if I will manage to do it?</p>            
         </div>
     </div>
 </div>
