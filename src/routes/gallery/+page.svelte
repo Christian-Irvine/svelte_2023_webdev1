564acf42786dd3ed52448d92e3495b6d869876ee
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2023-08-25 11:52:13 +1200

    Changed text on gallery

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,8 +1,8 @@
 <div class="gallery">
     <div>
-        <h2>Me Being Awesome</h2>
+        <h2>My Old Animations</h2>
         <div>
-            <img class="border" src="craftymcfish1.jpg" alt="Me being awesome">
+            <img class="border" src="craftymcfish1.jpg" alt="My Old Animations">
             <p class="center_vertically">This is an image of me on my first day of school, I was so exited and overjoyed to be going to school that day I nearly threw up!
                 However, I didn't throw up after all and made it to school, yipee!
             </p>            
@@ -10,9 +10,9 @@
     </div>
 
     <div>
-        <h2>Me Being Dope As</h2>
+        <h2>100 Days Challenege</h2>
         <div>
-            <img class="border" src="100DaysEpisode1.png" alt="Me being dope as">
+            <img class="border" src="100DaysEpisode1.png" alt="100 Days Challenege">
             <p class="center_vertically">This was my first day on my 100 Days Challenege run! Look how eager and ready I am to get started and win the game. I wonder if I will manage to do it?</p>            
         </div>
     </div>
