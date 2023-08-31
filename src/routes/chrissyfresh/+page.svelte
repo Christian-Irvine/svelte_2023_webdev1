c173a65afbdc4863938e3d457d65d800be0699f1
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2023-09-01 11:21:04 +1200

    Added some content to ChrissyFresh page

--- src/routes/chrissyfresh/+page.svelte
+++ src/routes/chrissyfresh/+page.svelte
@@ -4,16 +4,17 @@
     <div>
         <h3>Origins</h3>
         <p>
-            Chrissy Fresh started as the name I posted music under.
+            Chrissy Fresh started as the name I posted music under. And eventually became a name I went by online, for more personal things. 
+            For example my <a href="https://www.instagram.com/chrissy_fresh/?hl=en" target="_blank">Instagram</a> is called chrissy_fresh even though very little of what I post on there is about music. Its it just my online username for non gaming related things.
         </p>
     </div>
 
     <div>
         <h3>Classic Chrissy Fresh Songs:</h3>
         <ul>
-            <li>Luv or S</li>
-            <li>Discord</li>
-            <li>Steam</li>
+            <li><a href="https://youtu.be/k5HRtc6PMsI?si=b4F696BejiR7_Jt8" target="_blank">Luv or Somethin'</a></li>
+            <li><a href="https://youtu.be/FthGNI1uFwM?si=0Cv_aS8c6KGz4nw9" target="_blank">All I Want For Fortmas Is Vbucks</a></li>
+            <li><a href="https://youtu.be/YSANgdLCJyU?si=KoFYNeq4R0FCanyS" target="_blank">Empty House With An Open Door</a></li>
         </ul>
     </div>
 </div>
