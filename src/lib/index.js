fd225a1f469e522de898fe6b36f3036376349785
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2023-09-13 09:41:06 +1200

    Moved where minigame was

--- src/lib/index.js
+++
@@ -1,8 +0,0 @@
-let horse = "yipee";
-
-export { horse };
-
-let random = 0;
-let tries = 5;
-
-export { random, tries };
--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,22 +1,5 @@
 <script>
-    import { tries } from "$lib.index.js";
-    import { random } from "$lib.index.js";
-
-    let x = 0;
-    let z = 0;
-
-    function Method() {
-        x = x + 1;
-        console.log("Clicked " + x + " times")
-        z = Math.floor(Math.random() * 1000);
-    }
-
+    import Game from "$lib/TestGame.svelte" 
 </script>
     
-<h1>{ z }</h1>
-
-<div>
-    <button class="button" on:click={()=>Method()}>
-	    Click Me!
-    </button>
-</div>
+<Game />
