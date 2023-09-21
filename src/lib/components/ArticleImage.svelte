17d4a4abdeccc5c1804e3f8133206d0717b548cc
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2023-09-22 11:45:43 +1200

    Added gallry image component

---
+++ src/lib/components/ArticleImage.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let heading;
+    export let text;
+    export let image;
+</script>
+
+<div>
+    <div class="image-grid">
+        <div class="gallery-image" style="background-image:url({ image })"></div>
+        <div class="image-flex">
+            <h3>{ heading }</h3>
+            <p>{ text }</p>
+        </div>
+    </div>
+</div>
