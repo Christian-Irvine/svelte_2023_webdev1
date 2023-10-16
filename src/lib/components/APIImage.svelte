ca7b123cb3292b5a0f973e15294a0e6ae86a1767
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2023-10-17 11:42:50 +1300

    Moved gallery API image logic to a component

---
+++ src/lib/components/APIImage.svelte
@@ -0,0 +1,36 @@
+<script>
+    import { onMount } from "svelte";
+  
+      export let heading;
+      export let text;
+      export let image;
+      export let query;
+  
+      let setImageTextValues = (desc, alt_desc) => {
+          if (desc !== null) {
+              if (desc.length < alt_desc.length) {
+                  return;
+              }
+              else {
+                  heading = alt_desc;
+                  text = desc;
+              }
+          }
+          else {
+              heading = query;
+              text = alt_desc;
+          }
+      };
+  
+      onMount(() => {
+          setImageTextValues(heading, text);
+      });
+  </script>
+  
+  <div class="image-grid">
+      <div class="gallery-image" style="background-image:url({ image })"></div>
+      <div class="image-flex">
+          <h3>{ heading }</h3>
+          <p>{ text }</p>
+      </div>
+  </div>
