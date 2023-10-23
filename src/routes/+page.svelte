3b2052297b31785b254f01ad0c665fa110c64c70
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2023-10-24 11:26:40 +1300

    Started tyding up main page to line up with other pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,22 @@
+<script>
+    import Article from "$lib/components/Article.svelte";
+
+</script>
+
 <h1>Me</h1>
 
 <div class="page">
-    <h3>Hello!</h3>
-    <p id="hello">
-        My name is Christian, I have done many things under many names and this website is here to tell you about that.
-    </p>
-    <p>here is my horse variable:</p>
+    <Article
+        heading="Hello There!";
+        text="I am Christian and this website is all about me and my various usernames on the internet. From Chrissy Fresh and my music to CraftyMCFish and my minecraft youtube videos."
+    />
+
+    <Article
+        heading="CraftyMCFish";
+        text="CraftyMCFish is my gaming username on the internet.
+        It is what I play games under but it is also the name my Youtube Channel goes by.
+        On the channel I post Minecraft Gaming videos but plan to expand to other games in the future."
+    />
 </div>
 
 
