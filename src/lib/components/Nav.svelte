45e31e6cb894d32e53f1fde8501055a19fdcc6bc
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2023-10-27 11:00:17 +1300

    Added skeleton for hamburger menu functionality

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,12 +1,25 @@
-<nav>
-    <a href="/craftymcfish"><button class="headerbutton">Crafty</button></a>
-    <a href="/chrissyfresh"><button class="headerbutton">Chrissy</button></a>
-    <a href="/games"><button class="headerbutton">Games</button></a>
-    <a href="/gallery"><button class="headerbutton">Gallery</button></a>
-</nav>
-
 <script>
-    let changeNav = () => {
-        console.log("Resize time!");
+    import { onMount } from 'svelte';
+
+    
+
+    onMount(() => {
+
+    });
+
+    let ToggleHamburger = () => {
+        console.log("Toggled");
     }
 </script>
+
+<nav>
+    <div class="nav-buttons nav-hide">
+        <a href="/craftymcfish"><button class="headerbutton">Crafty</button></a>
+        <a href="/chrissyfresh"><button class="headerbutton">Chrissy</button></a>
+        <a href="/games"><button class="headerbutton">Games</button></a>
+        <a href="/gallery"><button class="headerbutton">Gallery</button></a>
+    </div>
+    <div class="icon-header">
+        <button class="hamburger-menu"><a on:click={ToggleHamburger}><i class="fa-solid fa-bars"></i></a></button>
+    </div>
+</nav>
