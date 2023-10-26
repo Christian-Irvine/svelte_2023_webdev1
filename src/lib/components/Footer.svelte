288d084c56e9e19dba8945d41f9ca33289b49b1d
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2023-10-27 10:59:31 +1300

    Updated footer icons

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,5 @@
 <footer>
-    <div class="icons">
+    <div class="icons-footer">
         <a href="https://www.instagram.com/chrissy_fresh/?hl=en"><i class="fa-brands fa-instagram"></i></a>
         <a href="https://www.youtube.com/c/CraftyMCFish"><i class="fa-brands fa-youtube"></i></a>
         <a href="https://steamcommunity.com/id/CraftyMCFish"><i class="fa-brands fa-steam"></i></a>
