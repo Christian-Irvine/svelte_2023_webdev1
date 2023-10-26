2e89f66a409e00ce60552137199435c4055855e1
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2023-10-27 11:08:10 +1300

    Added hamburger menu expanding

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,25 +1,33 @@
 <script>
     import { onMount } from 'svelte';
 
-    
+    let buttonDiv;
+    let hamburgerActive = false;
 
     onMount(() => {
-
+        buttonDiv = document.getElementsByClassName("nav-buttons")[0];
+        console.log(buttonDiv);
     });
 
     let ToggleHamburger = () => {
-        console.log("Toggled");
+        hamburgerActive = !hamburgerActive;
+        if (hamburgerActive) {
+            buttonDiv.classList.remove("nav-hide");
+        }
+        else {
+            buttonDiv.classList.add("nav-hide");
+        }
     }
 </script>
 
 <nav>
+    <div class="icon-header">
+        <button class="hamburger-menu"><a on:click={ToggleHamburger}><i class="fa-solid fa-bars"></i></a></button>
+    </div>
     <div class="nav-buttons nav-hide">
         <a href="/craftymcfish"><button class="headerbutton">Crafty</button></a>
         <a href="/chrissyfresh"><button class="headerbutton">Chrissy</button></a>
         <a href="/games"><button class="headerbutton">Games</button></a>
         <a href="/gallery"><button class="headerbutton">Gallery</button></a>
-    </div>
-    <div class="icon-header">
-        <button class="hamburger-menu"><a on:click={ToggleHamburger}><i class="fa-solid fa-bars"></i></a></button>
     </div>
 </nav>
