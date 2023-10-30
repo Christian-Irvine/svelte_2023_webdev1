67a3e29109860757f2d4c9853cf65f6768a6f0e8
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2023-10-31 11:46:15 +1300

    Added a few paragraphs of games in games page

--- src/routes/games/+page.svelte
+++ src/routes/games/+page.svelte
@@ -1,3 +1,60 @@
-<div class="page">
+<script>
+    import Article from "$lib/components/Article.svelte";
+    import ArticleImage from "$lib/components//ArticleImage.svelte";
+</script>
+
+<div class="title-div">
     <h1>Games</h1>
+</div>
+
+<div class="page">
+    <Article 
+        heading="Games I've Made"
+        text="Though I have never finished a game ive worked on, a few of them have come quite far and slowly over time they have increased in quality.
+        I started making games about 2 weeks before the Polytech Game Jam in 2022 before partaking in said Game Jam. 
+        I thought it would be fun to try and learn as much as possible in a short time and see if we could make anything playable.
+        From that I discovered making games is something I would love to do as a career and is something I hope to do."
+    />
+
+    <ArticleImage
+        heading="Hospital Hands"
+        text="Hospital Hands was the first game I had a hand in making and is the game my group made at the 2022 Polytech Game Jam.
+        The game was about you controlling your hands which were disconnected from your body.
+        In the abandoned hospital you had to go around and collect all the parts of your body and reconnect all your limbs.
+        It only broke like 50% of the time and for a first game in 2 days thats not too bad."
+        image="craftymcfish1.jpg"
+    />
+
+    <ArticleImage
+        heading="2D Fish Shooter"
+        text="2D Fish Shooter is a basic 2D game where your mouse controls a gun and you have to click (shoot) fish as they swim past. 
+        If too many fish get past you die.
+        As days go on they get faster and spawn more so eventually it gets too much.
+        Sadly the game got too hard to maintain because the code was abominbal so I eventually gave up."
+        image="craftymcfish1.jpg"
+    />
+
+    <ArticleImage
+        heading="James May and The Eight Cheeses"
+        text="God Save The Queen was a game I decided to make around the sad demise of The Queen (Rest In Peace).
+        You had to get the queen from one side of the map to the other where the Union Jack was without her falling or touching spikes or whatnot by placing things with a limited budget.
+        I didn't get very far because I didn't really enjoy making the game too much, and also the code was pretty hard to maintain."
+        image="craftymcfish1.jpg"
+    />
+
+    <ArticleImage
+        heading="God Save The Queen"
+        text="God Save The Queen was a game I decided to make around the sad demise of The Queen (Rest In Peace).
+        You had to get the queen from one side of the map to the other where the Union Jack was without her falling or touching spikes or whatnot by placing things with a limited budget.
+        I didn't get very far because I didn't really enjoy making the game too much, and also the code was pretty hard to maintain."
+        image="craftymcfish1.jpg"
+    />
+
+    <ArticleImage
+        heading="2D Fish Shooter 2"
+        text="2D Fish Shooter 2 is a lot like the first 2D Fish Shooter except I was slightly better at programming it when I started in 2023. 
+        This version got a lot further with a boss, a shop system, cooler animations, better pixel art, different guns e.t.c. 
+        The issue is after developing it for 6 months I once again got a lot better at programming and realised how the system wasn't great to maintain so moved on again."
+        image="craftymcfish1.jpg"
+    />
 </div>
