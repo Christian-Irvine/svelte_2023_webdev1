2243b5889355cf3101f7e5b4aceea70a11b87743
Christian Irvine <christian.irvine04@gmail.com>
2023-10-31 14:25:46 +1300

    Added commenting to header component

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,12 +1,16 @@
 <!-- The Header component with the nav inside and butons to navigate -->
 
 <script>
+    // Importing nav component to go inside
     import Nav from "$lib/components/Nav.svelte";
 
+    // importing what animation should play
     export let fadeClass;
 </script>
 
+<!-- Setting animation to either nothing, show the header, or hide the header -->
 <header class={ fadeClass }>
+    <!-- Top left icon button to bring you to home page -->
     <div class="title">
         <a href="/">
             <button>
@@ -14,5 +18,6 @@
             </button>
         </a>
     </div>
+    <!-- Putting Nav component into header -->
     <Nav/>
 </header>
