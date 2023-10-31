00864e92ccaa8fac35b327abfb66eea60da2be61
Christian Irvine <christian.irvine04@gmail.com>
2023-10-31 13:59:12 +1300

    Added commenting on Article component

--- src/lib/components/Article.svelte
+++ src/lib/components/Article.svelte
@@ -1,9 +1,12 @@
+<!-- The basic article component which had a title and a paragraph -->
+
 <script>
+    // Importing the variables for heading and paragraph
     export let heading;
     export let text;
 </script>
 
-
+<!-- The HTML that displays on the page -->
 <div>
     <h3>{ heading }</h3>
     <p>{ text }</p>
