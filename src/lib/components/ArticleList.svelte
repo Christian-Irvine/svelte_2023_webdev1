f6e708da5570c96172e8640cfa3865fc84ee4763
Christian Irvine <christian.irvine04@gmail.com>
2023-10-31 14:08:30 +1300

    Added commenting to ArticleList component

--- src/lib/components/ArticleList.svelte
+++ src/lib/components/ArticleList.svelte
@@ -1,10 +1,15 @@
+<!-- The component for taking in a list of items and displaying them -->
+
 <script>
+    // Importing heading and list items variables
     export let heading;
     export let items;
 </script>
 
+<!-- The HTML where it displays the heading and items -->
 <div>
     <h3>{ heading }</h3>
+    <!-- Looping over each item and displaying it as a list item in an unordered list -->
     <ul>
         {#each items as item}
             <li>{ item }</li>
