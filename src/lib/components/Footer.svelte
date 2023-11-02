bbc4fe795d4ad866d6e0c8d563e588ad276302a2
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2023-11-03 11:03:26 +1300

    Added footer link to Github Repo

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -7,5 +7,6 @@
         <a href="https://www.instagram.com/chrissy_fresh/?hl=en"><i class="fa-brands fa-instagram"></i></a>
         <a href="https://www.youtube.com/c/CraftyMCFish"><i class="fa-brands fa-youtube"></i></a>
         <a href="https://steamcommunity.com/id/CraftyMCFish"><i class="fa-brands fa-steam"></i></a>
+        <a href="https://github.com/Christian-Irvine/svelte_2023_webdev1"><i class="fa-brands fa-github"></i></a>
     </div>
 </footer>
