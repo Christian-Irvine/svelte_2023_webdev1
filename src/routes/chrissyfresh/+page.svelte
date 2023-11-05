1eae175df02a6e3db6903b7f890cf06562694eab
Christian-Irvine <christian.irvine04@gmail.com>
2023-11-06 10:08:09 +1300

    Updated Chrissy Fresh images to be higher quality

--- src/routes/chrissyfresh/+page.svelte
+++ src/routes/chrissyfresh/+page.svelte
@@ -34,27 +34,22 @@
         text="Luv or Somethin' was my first song I released for Valentines Day in 2020 (even if it was a few days late). It was created within a week with me writing the lyrics,
         and a friend of mine playing the guitar. We went over to another friend to record it one day, but all of the things we got didn't line up so we had to record it on our phones and send it to friend 2.
         And that abomination of a song creation week is half the reason it is so bad. The other reason was I didn't sing I just talked."
-        image="LuvOrSomethin.png"
+        image="ChrissyFresh/LuvOrSomethin.png"
     />
 
-    <Article 
-        heading="Luv or Somethin'" 
-        text="Luv or Somethin' was my first song I released for Valentines Day in 2020 (even if it was a few days late). It was created within a week with me writing the lyrics,
-        and a friend of mine playing the guitar. We went over to another friend to record it one day, but all of the things we got didn't line up so we had to record it on our phones and send it to friend 2.
-        And that abomination of a song creation week is half the reason it is so bad. The other reason was I didn't sing I just talked."
-    />
-
-    <Article 
+    <ArticleImage 
         heading="All I Want For Fortmas Is Vbucks" 
         text="My second song which I don't like to talk about is a Fortnite parody of the classic song ''All I Want For Christmas Is You'''. 
         I wrote it sometime in december one year and then never did much with it. Then in October I thought ''You know what'd be funny, if I made a Fortnite Christmas song in October''.
         So I did. I decided to play the ukulele as I wasn't very good at guitar at the time and it is what it is now."
+        image="ChrissyFresh/AllIWantForFortmasIsVbucks.jpg"
     />
 
-    <Article 
+    <ArticleImage 
         heading="Empty House With An Open Door" 
         text="This song I wrote in 2022, when I was getting slightly better at guitaring. Its a very deep song about love and loss. And 100% fictional which makes it very funny.
         in 2023 I decided I wanted another Valentines Day Song, so I went back to friend 2s house and went ham. This time though we both were more experienced and we actually produced an absolute banger.
         We even got an old friend I hadn't spoken to in over a decade to play the bass guitar (which slaps by the way)."
+        image="ChrissyFresh/EmptyHouseWithAnOpenDoor.png"
     />
 </div>
